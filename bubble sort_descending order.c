9325d1a31189a52078423d932023ed6e1bc17a46
Sinthuha Nadesan <nadasinthu09@gmail.com>
2025-05-18 18:42:12 +0530

    bubble sorting-descending order

---
+++ bubble sort_descending order.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int A[100], n, temp;
+
+    // Get number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    // Get the elements
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+    }
+
+for (int i=0;i<n-1;i++)
+{
+    for(int j=0;j<n-1-i;j++){
+        if(A[j]<A[j+1])
+        {
+            temp = A[j];
+            A[j] = A[j+1];
+            A[j+1] = temp;
+        }
+    }
+}
+   printf("Sorted array in descending order:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
